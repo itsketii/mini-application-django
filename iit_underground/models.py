746aa23a6e3cd3aaaf2789a5a0a28f342fbdd342
Ketsia <ketsia.kouadio@iit.ci>
2026-08-13 13:11:26 +0000

    Ajout des modèles Potin, Commentaire et Tag

--- iit_underground/models.py
+++ iit_underground/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    nom = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom
+
+
+class Potin(models.Model):
+    titre = models.CharField(max_length=200)
+    contenu = models.TextField()
+    image = models.ImageField(upload_to='potins/', blank=True, null=True)
+    auteur = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='potins'
+    )
+    anonyme = models.BooleanField(default=True)
+    date_publication = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='potins', blank=True)
+
+    def __str__(self):
+        return self.titre
+
+    class Meta:
+        ordering = ['-date_publication']
+
+
+class Commentaire(models.Model):
+    potin = models.ForeignKey(Potin, on_delete=models.CASCADE, related_name='commentaires')
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenu = models.TextField()
+    date_publication = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Commentaire de {self.auteur.username} sur {self.potin}'
+
+    class Meta:
+        ordering = ['-date_publication']
