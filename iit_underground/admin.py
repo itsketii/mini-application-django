7e3f1acffe94677348b6828bb61d64e552d33e20
Ketsia <ketsia.kouadio@iit.ci>
2026-08-13 13:18:07 +0000

    Configuration de l'interface admin et création du superuser

--- iit_underground/admin.py
+++ iit_underground/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Tag, Potin, Commentaire
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('nom',)
+
+
+@admin.register(Potin)
+class PotinAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'anonyme', 'date_publication')
+    list_filter = ('anonyme', 'tags')
+    search_fields = ('titre', 'contenu')
+
+
+@admin.register(Commentaire)
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('potin', 'auteur', 'date_publication')
